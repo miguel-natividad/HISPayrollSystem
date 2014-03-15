ef8da01c8e9345152c31818fde4ec431afaf67ae
Jourdan Christopher D. Go <jourdandgo@gmail.com>
2014-03-16 00:13:12 +0800

    updated' '
    1
    ;
    '

--- PredatorUI2/Import.Designer.cs
+++ PredatorUI2/Import.Designer.cs
@@ -42,7 +42,6 @@
             this.calculateBtn = new System.Windows.Forms.Button();
             this.curSalarySummaryBtn = new System.Windows.Forms.Button();
             this.manageEmployeesBtn = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -163,23 +162,12 @@
             this.manageEmployeesBtn.UseVisualStyleBackColor = true;
             this.manageEmployeesBtn.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button2
-            // 
-            this.button2.Location = new System.Drawing.Point(794, 126);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 38;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click_1);
-            // 
             // Import
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(984, 661);
-            this.Controls.Add(this.button2);
             this.Controls.Add(this.manageEmployeesBtn);
             this.Controls.Add(this.curSalarySummaryBtn);
             this.Controls.Add(this.calculateBtn);
@@ -216,7 +204,6 @@
         private System.Windows.Forms.Button calculateBtn;
         private System.Windows.Forms.Button curSalarySummaryBtn;
         private System.Windows.Forms.Button manageEmployeesBtn;
-        private System.Windows.Forms.Button button2;
     }
 }
 
--- PredatorUI2/Import.cs
+++ PredatorUI2/Import.cs
@@ -22,6 +22,7 @@ namespace PredatorUI2
 
         string projectID = "";
         string periodIDquery = "";
+        string timesheetEntryIDquery = "";
 
         public DataTable importedDT = new DataTable();
         public Import()
@@ -62,7 +63,72 @@ namespace PredatorUI2
                 Application.DoEvents();
             }
         }
+        public void getNextTimeSheetEntryID()
+        {
+            //Resets the String projectIDquery;
+            timesheetEntryIDquery = "";
 
+            MySqlConnection conn = new MySqlConnection(LogIn.login);
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand();
+
+            //Gets all the project_ids currently in the database
+            cmd.CommandText = "SELECT TSE_ID FROM entry_timesheet";
+            MySqlDataReader reader = cmd.ExecuteReader();
+
+            //Creates a list of integers which will hold the project IDs currently in the database 
+            List<int> projectIDnum = new List<int>();
+
+            if (reader.HasRows == true)
+            {
+                while (reader.Read())
+                {
+                    //Assigns to string 'word' the project_ID value
+                    string word = reader[0].ToString();
+
+                    //splits a project_id. From PRJ-000000000X into PRJ and 0000000000X
+                    string[] values = word.Split('-');
+
+                    //Adds only the numerical part to the list , 'projectIDnum'
+                    projectIDnum.Add(int.Parse(values[1]));
+
+                }
+                reader.Close();
+
+                int newNum = 0;
+
+                for (int k = 0; k < projectIDnum.Count(); k++)
+                {
+                    //checks if K has reached the LAST number in projectIDnum list
+                    if (k == projectIDnum.Count - 1)
+                    {
+                        //so the new number is equal to the LAST number incremented by 1.
+                        newNum += projectIDnum[k] + 1;
+                    }
+                }
+
+                //splits the new number into its individual characters, so that we can count how many zeroes we need.
+
+                int count = newNum.ToString().Length;
+
+                //begins the query by adding the prefix, 'PRJ-'
+                timesheetEntryIDquery += "TSE-";
+
+                //loop to decide how many zeros are needed before inputting the newNum
+                for (int k = 0; k < 10 - count; k++)
+                {
+                    timesheetEntryIDquery += "0";
+                }
+                timesheetEntryIDquery += newNum.ToString();
+
+                MessageBox.Show("The new project has been assigned with the Time Sheet Entry ID: " + timesheetEntryIDquery);
+
+            }
+            else
+            {
+                timesheetEntryIDquery = "TSE-0000000001";
+            }
+        }
         public void getNextPeriodID()
         {
             //Resets the String projectIDquery;
@@ -121,7 +187,7 @@ namespace PredatorUI2
                 }
                 periodIDquery += newNum.ToString();
 
-                MessageBox.Show("The new project has been assigned with the project ID: " + periodIDquery);
+                MessageBox.Show("The new project has been assigned with the period ID: " + periodIDquery);
 
             }
             else
@@ -130,7 +196,7 @@ namespace PredatorUI2
             }
         }
 
-        
+
         private void ImportFile()
         {
             if (txtFileName.Text.Trim() != string.Empty)
@@ -151,14 +217,15 @@ namespace PredatorUI2
 
                         //gets the Project Name and the start and end dates of the imported biometric file
                         string projectName = dataGridView1.Columns[0].Name.ToString();
-                        string startDate= dataGridView1.Rows[0].Cells[1].Value.ToString();
+                        //MessageBox.Show(projectName);
+                        string startDate = dataGridView1.Rows[0].Cells[1].Value.ToString();
                         string endDate = dataGridView1.Rows[1].Cells[1].Value.ToString();
 
                         //converts the date in the imported biometric file to a mysql accepted format
                         string[] startDateBroken = startDate.Split('/');
                         string correctlyFormattedStart = "";
                         correctlyFormattedStart += startDateBroken[2] + "-" + startDateBroken[0] + "-" + startDateBroken[1];
-                       
+
 
                         string[] endDateBroken = endDate.Split('/');
                         string correctlyFormattedEnd = "";
@@ -166,10 +233,10 @@ namespace PredatorUI2
 
 
 
-                    
+
                         //gets the next period ID 
                         getNextPeriodID();
-                        
+
                         //insert into period_table PERIOD ID, START_DATE, END_DATE based on imported biometric file
                         MySqlConnection conn = new MySqlConnection(LogIn.login);
                         conn.Open();
@@ -182,7 +249,7 @@ namespace PredatorUI2
 
 
                         //to compute for weeknum, month and year USING MYSQL 
-                       
+
 
                         cmd = conn.CreateCommand();
                         cmd.CommandText = "SELECT DATE_FORMAT(start_date, '%v') AS 'NUM', DATE_FORMAT(end_date, '%M') AS 'MONTH', DATE_FORMAT(end_date, '%Y')AS 'YEAR' FROM period_table WHERE period_id = @period_id";
@@ -192,15 +259,15 @@ namespace PredatorUI2
                         string month = "";
                         string year = "";
 
-                       while (reader.Read())
+                        while (reader.Read())
                         {
-                          weeknum = reader[0].ToString();
-                          month = reader[1].ToString();
-                          year = reader[2].ToString();
+                            weeknum = reader[0].ToString();
+                            month = reader[1].ToString();
+                            year = reader[2].ToString();
                         }
-                        MessageBox.Show(weeknum);
-                        MessageBox.Show(month);
-                        MessageBox.Show(year);
+                      //  MessageBox.Show(weeknum);
+                       // MessageBox.Show(month);
+                       // MessageBox.Show(year);
 
                         reader.Close();
                         /**DataTable query = new DataTable();
@@ -220,36 +287,82 @@ namespace PredatorUI2
                         cmd.Parameters.AddWithValue("@year", year);
                         cmd.Parameters.AddWithValue("@period_id", periodIDquery);
                         cmd.ExecuteNonQuery();
-                        MessageBox.Show("Updated");
+                        //MessageBox.Show("Updated");
 
 
                         //gets a list of employees
                         List<string> fullnamesEmployess = new List<string>();
 
-                         cmd = conn.CreateCommand();
-                        cmd.CommandText = "SELECT name_last, name_first FROM employee_table";
-                         reader = cmd.ExecuteReader();
+                        cmd = conn.CreateCommand();
+                        cmd.CommandText = "SELECT name_last, name_first, employee_ID FROM employee_table WHERE employee_table.project_ID =(SELECT project_ID FROM projects_table WHERE project_name =@project_name)";
+                        cmd.Parameters.AddWithValue("@project_name", projectName);
+                        reader = cmd.ExecuteReader();
 
                         while (reader.Read())
                         {
                             string fullname = "";
-                            fullname += reader[0].ToString() + ", " + reader[1].ToString();
+                            fullname += reader[0].ToString() + ", " + reader[1].ToString() + ", " + reader[2].ToString();
                             fullnamesEmployess.Add(fullname);
                         }
+
+                       
 
                         reader.Close();
 
                         //checks if member is found in the imported biometric file
                         bool memberFound = false;
-                        
-                        for (int k = 0; k < dataGridView1.Rows.Count-1; k++)
+
+                     
+
+                        for (int k = 0; k < dataGridView1.Rows.Count - 1; k++)
                         {
-                            foreach(string s in fullnamesEmployess)
+                            foreach (string s in fullnamesEmployess)
                             {
-                                if (dataGridView1.Rows[k].Cells[0].Value.ToString().Equals(s,StringComparison.OrdinalIgnoreCase))
+                                string[] value = s.Split(',');
+                                string onlythefullname = "";
+                                string employee_ID = value[2];
+                                onlythefullname += value[0] + "," + value[1];
+                                
+                          
+                                if (dataGridView1.Rows[k].Cells[0].Value.ToString().Equals(onlythefullname.ToUpper(), StringComparison.OrdinalIgnoreCase))
                                 {
-                                    MessageBox.Show("Employee found: " + dataGridView1.Rows[k].Cells[0].Value.ToString());
+                                    //MessageBox.Show("Employee found: " + dataGridView1.Rows[k].Cells[0].Value.ToString());
                                     memberFound = true;
+
+                                   
+                                    
+
+                                    for (int sk = 1; sk <= 6; sk++)
+                                    {
+                                            cmd = conn.CreateCommand();
+                                            cmd.CommandText = "INSERT INTO entry_timesheet(TSE_ID, date, morning_in, morning_out, afternoon_in, afternoon_out, ot_in, ot_out, period_ID, project_ID, employee_ID) VALUES (@TSE_ID, @date, @morning_in, @morning_out, @afternoon_in, @afternoon_out, @ot_in, @ot_out, @period_ID, @project_ID, @employee_ID)";
+                                            //gets the next ID for Time Sheet Entry table
+                                            getNextTimeSheetEntryID();
+                                            cmd.Parameters.AddWithValue("@TSE_ID", timesheetEntryIDquery);
+
+                                            string dateEntryWithoutDay = dataGridView1.Rows[k + sk].Cells[0].Value.ToString();
+                                            string[] justDate = dateEntryWithoutDay.Split(' ');
+                                            string[] individualDateElements = justDate[0].Split('/');
+                                            string finaldateEntry = "";
+                                            finaldateEntry += individualDateElements[2] + "-" + individualDateElements[0] + "-" + individualDateElements[1];
+                                            //MessageBox.Show(finaldateEntry);
+                                            cmd.Parameters.AddWithValue("@date", finaldateEntry);
+                                            cmd.Parameters.AddWithValue("@morning_in", dataGridView1.Rows[k + sk].Cells[1].Value.ToString());
+                                            cmd.Parameters.AddWithValue("@morning_out", dataGridView1.Rows[k + sk].Cells[2].Value.ToString());
+                                            cmd.Parameters.AddWithValue("@afternoon_in", dataGridView1.Rows[k + sk].Cells[3].Value.ToString());
+                                            cmd.Parameters.AddWithValue("@afternoon_out", dataGridView1.Rows[k + sk].Cells[4].Value.ToString());
+                                            cmd.Parameters.AddWithValue("@ot_in", dataGridView1.Rows[k + sk].Cells[5].Value.ToString());
+                                            cmd.Parameters.AddWithValue("@ot_out", dataGridView1.Rows[k + sk].Cells[6].Value.ToString());
+                                            cmd.Parameters.AddWithValue("@period_ID", periodIDquery);
+                                            cmd.Parameters.AddWithValue("@project_ID", projectID);
+                                            //  MessageBox.Show(projectID);
+                                            cmd.Parameters.AddWithValue("@employee_ID", employee_ID);
+                                            // MessageBox.Show(employee_ID);
+
+                                            cmd.ExecuteNonQuery();
+
+                                          
+                                    }
                                 }
                             }
                         }
@@ -259,11 +372,11 @@ namespace PredatorUI2
                             MessageBox.Show("No such member found");
                         }
 
-                       /** foreach (string s in fullnamesEmployess)
-                        {
-                            MessageBox.Show(s);
-                        }*/
-
+                        /** foreach (string s in fullnamesEmployess)
+                         {
+                             MessageBox.Show(s);
+                         }*/
+                        MessageBox.Show("import complete");
                     }
                 }
                 catch (Exception ex)
@@ -317,46 +430,6 @@ namespace PredatorUI2
             emppanel.Show();
         }
 
-        private void button2_Click_1(object sender, EventArgs e)
-        {
-            //to compute for weeknum, month and year USING MYSQL 
-            MySqlConnection conn = new MySqlConnection(LogIn.login);
-            conn.Open();
-          
-            MySqlCommand cmd = conn.CreateCommand();
-            cmd.CommandText = "SELECT DATE_FORMAT(start_date, '%v') AS 'NUM', DATE_FORMAT(end_date, '%M') AS 'MONTH', DATE_FORMAT(end_date, '%Y')AS 'YEAR' FROM period_table WHERE period_id = @period_id";
-            cmd.Parameters.AddWithValue("@period_id", periodIDquery);
-            MySqlDataReader reader = cmd.ExecuteReader();
-            string weeknum = "";
-            string month = "";
-            string year = "";
-            
-            /**while (reader.Read())
-            {
-              weeknum = reader[0].ToString();
-              month = reader[1].ToString();
-              year = reader[2].ToString();
-            }
-            MessageBox.Show(weeknum);
-            MessageBox.Show(month);
-            MessageBox.Show(year);*/
-
-            DataTable query = new DataTable();
-            query.Load(reader);
-            dataGridView1.DataSource = query;
-            reader.Close();
-
-            weeknum = dataGridView1.Rows[0].Cells[0].Value.ToString();
-            month = dataGridView1.Rows[0].Cells[1].Value.ToString();
-            year = dataGridView1.Rows[0].Cells[2].Value.ToString();
-            //updating  period_table the computed values of WEEKNUM, MONTH, YEAR
-            cmd = conn.CreateCommand();
-            cmd.CommandText = "UPDATE period_table SET week_num = @week_num, month = @month, year = @year WHERE period_ID = @period_ID";
-            cmd.Parameters.AddWithValue("@week_num", weeknum);
-            cmd.Parameters.AddWithValue("@month", month);
-            cmd.Parameters.AddWithValue("@year", year);
-            cmd.Parameters.AddWithValue("@period_id", periodIDquery);
-            cmd.ExecuteNonQuery();
-        }
+        
     }
 }
